6960fc35c2519d8f74f26a78a3e6641b967d0c61
Siddharth Ahluwalia <siddharth.ahluwalia16@gmail.com>
2023-06-11 22:17:33 -0400

    [FLASK] intro to flask basicss

---
+++ flask/1task-list/env/app.py
@@ -0,0 +1,15 @@
+#virtualenv env
+#pip3 install flask flask-sqlalchemy
+#python3 app.py
+#go to link
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+        app.run(debug=True)
