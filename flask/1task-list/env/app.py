0c330b8f1f90d1f03494bbc13876b48d4e578b66
Siddharth Ahluwalia <siddharth.ahluwalia16@gmail.com>
2023-06-14 17:29:08 -0400

    [FLASK] add POST route + db operations

--- flask/1task-list/env/app.py
+++ flask/1task-list/env/app.py
@@ -13,7 +13,7 @@
 #app.app_context().push()
 #db.create_all()
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,18 +21,33 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo (db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+    # completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %I' %self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
+
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "error: issue adding your task"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
 
 if __name__ == "__main__":
         app.run(debug=True)
