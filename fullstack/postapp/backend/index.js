a2ca5c57c18050bb41ac625e19b9404709c20e92
Siddharth Ahluwalia <siddharth.ahluwalia16@gmail.com>
2023-10-12 18:56:33 -0400

    [FULLSTACK] add a database and posts table to the app using ORM

--- fullstack/postapp/backend/index.js
+++ fullstack/postapp/backend/index.js
@@ -6,9 +6,28 @@
     // refer to package.json scripts
     // npm start
 
+// for database
+    // download mysql server
+    // download mysql workbench
+    // create new schema in workbench
+    // npm install -g sequelize sequelize-cli
+    // sequelize init
+    // this will create a bunch of files and dirs
+    // create new file under models/ for whatever table you want to create
+    // define the table within this file along with the columns you want it to have (function postsinit)
+    // setup config/config.json
+    // import db into index.js
+
+// basic bare minimum setup
 const express = require('express')
 const app = express()
 
 app.listen(3001, () => {
     console.log("TEST: server running on port 3001")
+});
+
+// import db into index.js. sequelize.sync creates the table in db if not created
+const db = require("./models")
+db.sequelize.sync().then(() =>{
+    console.log("initialize posts table in DB")
 });
---
+++ fullstack/postapp/backend/models/posts.js
@@ -0,0 +1,11 @@
+// const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define("Posts", {
+        title:{type:DataTypes.STRING, allowNull: false},
+        posttext:{type:DataTypes.STRING, allowNull: false},
+        username:{type:DataTypes.STRING, allowNull: false}
+
+    });
+    return Posts;
+}
