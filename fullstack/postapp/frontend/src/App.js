48f858a5936e301533b65b58931f1d1d6eb806da
Siddharth Ahluwalia <siddharth.ahluwalia16@gmail.com>
2023-10-13 22:03:42 -0400

    [FULLSTACK] initial commit and dir structure for frontend dev

---
+++ fullstack/postapp/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// add basic bare minimum code
+  // go to front-end and type npx create-react-app .
+  // delete unneccessary files and type npm start
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+
+export default App;
