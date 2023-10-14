972de74b2dcab1713a6b48576c2cca2374d1a8d3
Siddharth Ahluwalia <siddharth.ahluwalia16@gmail.com>
2023-10-13 22:57:16 -0400

    [FULLSTACK] connect BE to FE using cors and axios
    - also add styling to App.css

--- fullstack/postapp/frontend/src/App.js
+++ fullstack/postapp/frontend/src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react"; //logic to allow reload a func at page load
 
 // add basic bare minimum code
   // go to front-end and type npx create-react-app .
   // delete unneccessary files and type npm start
+  // npm install axios (alternate to fetch)
+
+// 5. connect backend to frontend
+  // navigate to backend dir and install cors
+  // 5.1 add code to white list and connect FE
+  // npm install axios
+  // 5.2 add code to extract posts from db using axios
+
+// 6. react front-end coding
+  // 6.1 add code to display posts from database
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [listofposts, setlistofposts] = useState([]);
+
+   // 5.2
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setlistofposts(response.data);
+    });
+  }, []);
+
+  return (
+  <div className="App"> {
+    
+    // 6.1
+    listofposts.map((value, key) => { return (
+    <div className="post"> 
+      <div className="title"> {value.title} </div>
+      <div className="body"> {value.posttext} </div>
+      <div className="footer"> {value.username} </div>
+    </div>);
+  }
+
+  )} 
+  </div>
   );
 }
 
